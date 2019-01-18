e0eb3b513a3486ce33c6dc79a43e8af17ae5277a
NokiDev <noki.dev@gmail.com>
2019-01-18 10:37:18 +0000

    TMP

--- main.py
+++ main.py
@@ -6,31 +6,40 @@ from bluetooth import *
 
 ## CONFIG:
 ID = "1"
-HOST = "B8:27:EB:40:D6:34"#sys.argv[1]
-PORT = 3
-TIME = 2
+HOST = "B8:27:EB:40:D6:34" if not len(sys.argv) == 2 else sys.argv[1]
+PORT = 3 if not len(sys.argv) == 3 else sys.argv[2]
+TIME = 60 if not len(sys.argv) == 4 else sys.argv[3]
 
+## Open bluetooth socket, and connect to HOST, and send the message in parameters.
+def send_message_once(message):
+	try :
+		s=BluetoothSocket( RFCOMM )
+		print("Connect to {}:{}".format(HOST, PORT))
+		s.connect((HOST, PORT))
+		s.send(message)
+		s.close()
+	except :
+		err = sys.exc_info()[0]
+		print("Unexpected disconnect REASON : {}".format(err))
 
+## Run the main process.
 def run():
 	print("Init System")
-	#sensor.init()
-	while True:
-		print("Checking Temperature")
-		humidity, temperature = sensor.check_temperature()
-		timestamp = time.time()
-		message = json.JSONEncoder().encode({"temperature": temperature, "humidity": humidity, "timestamp": timestamp})
-        #Connect temporarly when sending the data overbluetooth
-		try :
-			s=BluetoothSocket( RFCOMM )
-			print("Connect to {}:{}".format(HOST, PORT))
-			s.connect((HOST, PORT))
-			s.send(message)
-			s.close()
-		except :
-			err = sys.exc_info()[0]
-			print("Unexpected disconnect REASON : {}".format(err))
-
-		time.sleep(TIME)
+	try:
+		sensor.init()
+		while True:
+			print("Checking Temperature")
+			humidity, temperature = sensor.check_temperature()
+			timestamp = time.time()
+			message = json.JSONEncoder().encode({"id": ID, "temperature": temperature, "humidity": humidity, "timestamp": timestamp})
+			#Connect temporarly when sending the data overbluetooth
+			send_message_once(message)
+			elapsedTime = time.time() - timestamp
+			timeToWait = TIME - elapsedTime if TIME - elapsedTime > 0 else 0 
+			time.sleep(TIME)
+	except KeyboardInterrupt:
+		print("QUITTING")
+	sensor.deinit()
 
 if __name__ == "__main__" :
 	run()
--- sensor.py
+++ sensor.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # coding: utf-8
 import sys
+import time
+import RPi.GPIO as GPIO
 import Adafruit_DHT
 
 # parameters
@@ -9,8 +11,22 @@ Temperature_GPIO = 23
 LED_GPIO = 24
 BUTTON_GPIO = 21
 
+def init():
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(LED_GPIO, GPIO.OUT)
+	GPIO.output(LED_GPIO, GPIO.HIGH)
+	GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.add_event_detect(BUTTON_GPIO, GPIO.FALLING, callback=check_temperature, bouncetime=200)
+
+
 def check_temperature():
 	# read and report dht11 temperature and humidity
+	GPIO.output(LED_GPIO, GPIO.HIGH)
 	humidity, temperature = Adafruit_DHT.read_retry(DHT_type, Temperature_GPIO)
 	print("Humidity {}, Temperature {}".format(humidity, temperature))
+	GPIO.output(LED_GPIO, GPIO.LOW)
 	return humidity, temperature
+
+def deinit():
+	GPIO.output(LED_GPIO, GPIO.HIGH)
+	GPIO.cleanup()
